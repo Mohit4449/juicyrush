0dc86734d7b6f21cbcc0f62b29f8f91ef61f3686
Mohit4449 <mohitkanjariya07@gmail.com>
2025-07-19 06:48:38 +0530

    a

--- home.php
+++ home.php
@@ -13,7 +13,7 @@ if (isset($_SESSION['role']) && $_SESSION['role'] === 'admin') {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Juicy Rush - Home</title>
+  <title>Juicy Rush-Home</title>
   <link rel="stylesheet" href="style/homestyles.css">
 </head>
 <body>
